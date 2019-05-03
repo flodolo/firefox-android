2657a2e8639d5cc1988fc5343fbda6adeb6574fe
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-05-03 16:40:22 +0200

    [fenix] Generate the `activation` ping and send it with Glean (https://github.com/mozilla-mobile/fenix/pull/1707)
    This fetches the Google Advertising ID, salts it and
    then applies hashing before sending a ping with it,
    at startup. Hashing and salting are used in order
    to prevent ourselves to correlate advertising IDs
    from the same user running different products we
    own off a single device. We will never send the
    client_id and the Google Advertising ID in the same
    ping.

--- fenix/buildSrc/src/main/java/Dependencies.kt
+++ fenix/buildSrc/src/main/java/Dependencies.kt
@@ -164,4 +164,6 @@ object Deps {
     const val tools_test_rules = "com.android.support.test:rules:${Versions.tools_test_rules}"
     const val tools_test_runner = "com.android.support.test:runner:${Versions.tools_test_runner}"
     const val uiautomator = "com.android.support.test.uiautomator:uiautomator-v18:${Versions.uiautomator}"
+
+    const val google_ads_id = "com.google.android.gms:play-services-ads-identifier:${Versions.google_ads_id_version}"
 }
