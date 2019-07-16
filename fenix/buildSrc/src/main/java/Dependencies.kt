3268706dd79ca92991793500125ee8f5ce0f7ace
Sebastian Kaspari <s.kaspari@gmail.com>
2019-07-16 11:58:07 +0200

    [fenix] Use Mozilla Android Components 4.0.0.

--- fenix/buildSrc/src/main/java/Dependencies.kt
+++ fenix/buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-private object Versions {
+object Versions {
     const val kotlin = "1.3.30"
     const val coroutines = "1.2.1"
     const val android_gradle_plugin = "3.4.1"
