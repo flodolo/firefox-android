ef3bb49831447e5d006d8969cfa97b417b431661
Sebastian Kaspari <s.kaspari@gmail.com>
2019-09-12 11:56:44 +0200

    [fenix] Unbreak Nightly: Get rid of fragment-testing dependency and create fragments in test manually.

--- fenix/buildSrc/src/main/java/Dependencies.kt
+++ fenix/buildSrc/src/main/java/Dependencies.kt
@@ -207,7 +207,6 @@ object Deps {
     const val fenix_megazord_forUnitTests = "org.mozilla.appservices:fenix-megazord-forUnitTests:${Versions.mozilla_appservices}"
 
     const val google_ads_id = "com.google.android.gms:play-services-ads-identifier:${Versions.google_ads_id_version}"
-    const val androidx_fragment_testing = "androidx.fragment:fragment-testing:${Versions.androidx_fragment_testing_version}"
 
     const val lottie = "com.airbnb.android:lottie:${Versions.airbnb_lottie}"
 }
